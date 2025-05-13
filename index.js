59164dbab2dd2b92f2f873e7c3d60e5743e096f7
afzzrdn <mhmdafz78@gmail.com>
2025-05-13 08:29:50 +0700

    fix: error filter badwords 2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.MessageContent,
     ],
