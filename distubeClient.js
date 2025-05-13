e111937404df1d9219d0c3d00d857d1c59ea2338
afzzrdn <mhmdafz78@gmail.com>
2025-05-13 21:14:53 +0700

    fix: play music 5

--- distubeClient.js
+++ distubeClient.js
@@ -2,12 +2,12 @@ const { DisTube } = require('distube');
 const { YtDlpPlugin } = require('@distube/yt-dlp');
 const ffmpeg = require('@ffmpeg-installer/ffmpeg');
 
-process.env.FFMPEG_PATH = ffmpeg.path; 
+// Set path ke FFmpeg
+process.env.FFMPEG_PATH = ffmpeg.path;
 
 const createDistube = (client) => {
   return new DisTube(client, {
     plugins: [new YtDlpPlugin()],
-    ffmpeg: ffmpeg.path, // Optional, just to be extra safe
   });
 };
 
