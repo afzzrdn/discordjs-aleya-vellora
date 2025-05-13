57f849b659297005a6307631760f02b7efd3cddd
afzzrdn <mhmdafz78@gmail.com>
2025-05-13 18:37:21 +0700

    fix: play music 2

--- distubeClient.js
+++ distubeClient.js
@@ -3,12 +3,16 @@ const { DisTube } = require('distube');
 const { YtDlpPlugin } = require('@distube/yt-dlp');
 const ffmpegPath = require('ffmpeg-static');
 
-// ⛑️ Set path ffmpeg secara global (wajib untuk Railway dan host tanpa ffmpeg)
+// Set path ffmpeg untuk DisTube dan yt-dlp
 process.env.FFMPEG_PATH = ffmpegPath;
 
 const createDistube = (client) => {
   return new DisTube(client, {
-    plugins: [new YtDlpPlugin()],
+    plugins: [new YtDlpPlugin({
+      update: false,
+      ffmpeg: ffmpegPath,
+    })],
+    ffmpeg: ffmpegPath,
   });
 };
 
