dfb82e90dd9c8c62e82fbb51d3dbf8978ef6ead4
afzzrdn <mhmdafz78@gmail.com>
2025-05-15 21:29:17 +0700

    feat: mabar 4

--- commands/general/mabar.js
+++ commands/general/mabar.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { LOGS_CHANNEL_ID } = require('../config/ids');
+const { LOGS_CHANNEL_ID } = require('../../config/ids');
 
 module.exports = {
     data: new SlashCommandBuilder()
